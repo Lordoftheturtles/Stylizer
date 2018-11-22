dc6867a3f3f49757783a35622cdc47cd6506159d
Brad Seymour <imnotham2012@brad-Seymours-iMac.local>
2018-11-22 21:52:58 +1100

    Stylizer Update 1.2

--- MAINVC/ViewController.swift
+++ MAINVC/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 import CoreData
 import Firebase
+import FirebaseAuth
 
 
 class ViewController: UIViewController {
+let context = PersistenceServce.context
+    var user: [User] = []
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        hideKeyboard()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,6 +29,45 @@ class ViewController: UIViewController {
         
         
     }
+
+    //MARK: LOGIN FUNCTION AUTHORISATION
+    // IS THE USER LOGGED IN??
+   // if Auth.auth().currentUser != nil {
+    //let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //let _ = storyboard.instantiateViewController(withIdentifier: "homeViewController") as? UINavigationController
+   // performSegue(withIdentifier: "logInSegue", sender: nil)
+    //} else {
+   // let storyboard = UIStoryboard(name: "Main", bundle: nil)
+   // storyboard.instantiateInitialViewController()
+  //  }
+    
+    
+    //CONFIRM BUTTON TAPPED
+//    guard let email = emailTextField.text,
+//    email != "",
+//    let password = passwordTextField.text,
+//    password != ""
+//    else {
+//    AlertController.showCustomAlert(self, title: "Missing information", message: "Please fill out the required fields!")
+//    return
+//    }
+//
+//    Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+//    guard error == nil else {
+//    AlertController.showCustomAlert(self, title: "Error", message: error!.localizedDescription)
+//    return
+//    }
+//    guard let user = user else { return }
+//    print(user.user.email ?? "Missing Email")
+//    print(user.user.displayName ?? "Missing Display Name")
+//    print(user.user.uid)
+//    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//    let _ = storyboard.instantiateViewController(withIdentifier: "homeViewController") as? UINavigationController
+//    self.performSegue(withIdentifier: "logInSegue", sender: nil)
+//    }
+//    )}
+    
+   
 // Loginviewcontroller
 
     
@@ -31,16 +75,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginLines: UIImageView!
     @IBOutlet weak var blueBox: UIImageView!
     @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var orText: UIImageView!
     
+    @IBOutlet weak var enterEmailText: UITextField!
+   
     
+    @IBOutlet weak var enterPasswordText: UITextField!
+   
+    //DISMISS THE KEYBOARD
+  
+
+    
+
+
     
     @IBAction func continueToMain(_ sender: UIButton) {
         // Continues to Main Screen
         performSegue(withIdentifier: "goHome", sender: self)
         print("Going to Home Screen!")
-        
-
     }
+    
+    @IBAction func emailButtonTapped(_ sender: Any) {
+        
+        
+        print("Email label began editing")
+    }
+    
+    @IBAction func passwordButtonTapped(_ sender: Any) {
+        
+        
+        print("Password Label began editing")
+    }
+    
+    
+    
     
     func animations() {
         //Changes the alpha of the login Screen when displayed
@@ -71,8 +139,70 @@ class ViewController: UIViewController {
         self.continueButton.alpha = 1
         
         })
+        
+        UIView.animate(withDuration: 3, delay: 3.1, options: .curveEaseIn, animations:
+            {
+            self.orText.alpha = 1
+        })
+        UIView.animate(withDuration: 3, delay: 3.1, options: .curveEaseIn, animations:
+            {
+                self.enterEmailText.alpha = 1
+        })
+        
+        UIView.animate(withDuration: 3, delay: 3.1, options: .curveEaseIn, animations:
+            {
+                self.enterPasswordText.alpha = 1
+        })
+        
+        
         print("Function is working?")
         
-    }
 }
+    //SIGNUP TBA
+    //let email = emailTextField.text,
+    //email != "",
+    //let password = passwordTextField.text,
+    //password != ""
+    //else {
+    //AlertController.showCustomAlert(self, title: "Missing Information", message: "Please fill in the required fields")
+    //return
+    
+   // }
+    //Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+    
+   // guard error == nil else {
+   // AlertController.showCustomAlert(self, title: "Error", message: error!.localizedDescription)
+   // return
+  //  }
+   // guard let user = user else { return }
+  //  print(user.user.email ?? "Mising Email")
+   // print(user.user.uid)
+  //  let changeRequest = user.user.createProfileChangeRequest()
+  //  changeRequest.displayName = username
+  //  changeRequest.commitChanges(completion: { (error) in
+    //guard error == nil else {
+    //AlertController.showCustomAlert(self, title: "Error", message: error!.localizedDescription)
+    //return
+   //}
+   // let storyboard = UIStoryboard(name: "Main", bundle: nil)
+   // let _ = storyboard.instantiateViewController(withIdentifier: "homeViewController") as? UINavigationController
+   // self.performSegue(withIdentifier: "registerSegue", sender: nil)
+   // }
+   // )  })
+    
+//}
 
+
+}
+extension UIViewController {
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
