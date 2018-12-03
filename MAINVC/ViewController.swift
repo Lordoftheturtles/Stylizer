83e7098a063b12cd0638ebd06757dc288db7ee44
Bradley Seymour <stylizerteam@Bradleys-MBP.gateway>
2018-12-03 22:17:30 +1100

    Added Login Screen + Animations

--- MAINVC/ViewController.swift
+++ MAINVC/ViewController.swift
@@ -17,139 +17,63 @@ class ViewController: UIViewController {
 
     var user: [User] = []
 
-    @IBOutlet weak var loginButton : FBSDKLoginButton!
+    @IBOutlet weak var stylizerLogo: UIImageView!
+    @IBOutlet weak var loginDetailsBox: UIImageView!
+    @IBOutlet weak var loginIcons: UIImageView!
+    @IBOutlet weak var forgotPasswordButton: UIButton! // 2
+    @IBOutlet weak var loginButton: UIButton! // 3
+    @IBOutlet weak var registerButton: UIButton! //3
+    
+    
+    
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        hideKeyboard()
-        //self.loginButton.delegate = self
-        if (FBSDKAccessToken.current() != nil) {
-    // User is logged in, use 'accessToken' here.
-        } else {
-       // User is logged out (ERROR)
-        }
+        dismissKeyboard()
+        animateLoginScreen()
+    }
      
         
+  
         
-        
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.frame.origin.y = 220
-        loginButton.frame.origin.x = 101
-        view.addSubview(loginButton)
-        loginButton.isHidden = true
-        
-     
-        
-        
-            }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        animations()
         
-        if Auth.auth().currentUser != nil {
-            let storyboard = UIStoryboard(name: "", bundle: nil)
-            let _ = storyboard.instantiateViewController(withIdentifier: "") as? UINavigationController
-            performSegue(withIdentifier: "", sender: nil)
-        } else {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            storyboard.instantiateInitialViewController()
+        
         }
+    
+    func animateLoginScreen() {
+        
+        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn, animations: {
+            self.stylizerLogo.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 1.5, options: .curveEaseIn, animations: {
+            self.loginDetailsBox.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 1.5, options: .curveEaseIn, animations: {
+            self.emailAddressTextField.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 1.5, options: .curveEaseIn, animations: {
+            self.passwordTextField.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 2.5, options: .curveEaseIn, animations: {
+            self.loginIcons.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 3, options: .curveEaseIn, animations: {
+            self.forgotPasswordButton.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 3.5, options: .curveEaseIn, animations: {
+            self.loginButton.alpha = 1
+        })
+        UIView.animate(withDuration: 2, delay: 3.5, options: .curveEaseIn, animations: {
+            self.registerButton.alpha = 1
+        })
+        print("Login Screen is animating!")
         
     }
-    
-    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("User Logged In")
-        
-        if ((error) != nil)
-        {
-            // Process error
-        }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // If you ask for multiple permissions at once, you
-            // should check if specific permissions missing
-            if result.grantedPermissions.contains("email")
-            {
-                // Do work
-            }
-        }
-    }
-    
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        print("User Logged Out")
-    }
-
-    
-    
-// Loginviewcontroller
-
-    
-    @IBOutlet weak var stylogo: UIImageView!
-        @IBOutlet weak var blueBox: UIImageView!
-    @IBOutlet weak var continueButton: UIButton!
-    @IBOutlet weak var facebookBackDrop: UIImageView!
-    
-
-    
-    @IBAction func continueToMain(_ sender: UIButton) {
-        // Continues to Main Screen
-        performSegue(withIdentifier: "goHome", sender: self)
-        print("Going to Home Screen!, Welcome Guest")
-    }
-//WIP
-  //  func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, //error: NSError!) {
-     //   if ((error) != nil) {
-            // Process error
-     //   }
-     //   else if result.isCancelled {
-            // Handle cancellations
-     //   }
-      //  else {
-            // Navigate to other view
-       // }
-    
-    
-    
-    func animations() {
-        //Changes the alpha of the login Screen when displayed
-        //Animated the login screen
-        
-        UIView.animate(withDuration: 2.5, delay: 2, options: .curveEaseIn, animations:
-            {
-            self.stylogo.alpha = 1
-        
-        })
-    
-        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn, animations:
-            {
-                self.blueBox.alpha = 1
-                
-        })
-        
-        UIView.animate(withDuration: 3, delay: 3, options: .curveEaseIn, animations:
-        {
-        self.continueButton.alpha = 1
-        
-        })
-        
-        UIView.animate(withDuration: 3, delay: 2, options: .curveEaseIn, animations: {
-            self.facebookBackDrop.alpha = 1
-        })
-        
-        UIView.animate(withDuration: 3, delay: 2, options: .curveEaseIn, animations: {
-            self.continueButton.alpha = 1
-            
-        })
-        UIView.animate(withDuration: 3, delay: 2, animations: {
-       //
-        })
-        
-        print("Function is working?")
-}
-  
-    
     
 }
 
